d23766623862293b0a483ff77f394cfa3a331cbc
BJ Miller <bj@sixfivesoftware.com>
2014-05-03 09:12:52 -0400

    Initial Commit

---
+++ RollPitchYaw/RPYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RPYAppDelegate.h
+//  RollPitchYaw
+//
+//  Created by BJ Miller on 5/3/14.
+//  Copyright (c) 2014 Six Five Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RollPitchYawTests/RollPitchYawTests.m
@@ -0,0 +1,34 @@
+//
+//  RollPitchYawTests.m
+//  RollPitchYawTests
+//
+//  Created by BJ Miller on 5/3/14.
+//  Copyright (c) 2014 Six Five Software, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RollPitchYawTests : XCTestCase
+
+@end
+
+@implementation RollPitchYawTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
