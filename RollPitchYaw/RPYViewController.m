8d64e82d44bcae86786523fa1745685ab873ab0d
BJ Miller <bj@sixfivesoftware.com>
2014-05-20 19:31:04 -0400

    initial push of working app to get roll, pitch, yaw, and gyro data.

--- RollPitchYaw/RPYViewController.m
+++ RollPitchYaw/RPYViewController.m
@@ -7,23 +7,53 @@
 //
 
 #import "RPYViewController.h"
+@import CoreMotion;
+
+#define kRadToDeg   57.2957795
 
 @interface RPYViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *rollLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pitchLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yawLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rotationXLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rotationYLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rotationZLabel;
+@property (nonatomic, strong) CMMotionManager *motionManager;
 @end
 
 @implementation RPYViewController
 
+- (CMMotionManager *)motionManager
+{
+    if (!_motionManager) {
+        _motionManager = [CMMotionManager new];
+        [_motionManager setDeviceMotionUpdateInterval:0.01];
+    }
+    return _motionManager;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMDeviceMotion *motion, NSError *error) {
+        self.rollLabel.text = [NSString stringWithFormat:@"%.2gº", motion.attitude.roll * kRadToDeg];
+        self.pitchLabel.text = [NSString stringWithFormat:@"%.2gº", motion.attitude.pitch * kRadToDeg];
+        self.yawLabel.text = [NSString stringWithFormat:@"%.2gº", motion.attitude.yaw * kRadToDeg];
+    }];
+    
+    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMGyroData *gyroData, NSError *error) {
+        self.rotationXLabel.text = [NSString stringWithFormat:@"%f", gyroData.rotationRate.x];
+        self.rotationYLabel.text = [NSString stringWithFormat:@"%f", gyroData.rotationRate.y];
+        self.rotationZLabel.text = [NSString stringWithFormat:@"%f", gyroData.rotationRate.z];
+    }];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewWillDisappear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.motionManager stopDeviceMotionUpdates];
+    self.motionManager = nil;
+    [super viewWillDisappear:animated];
 }
 
 @end
